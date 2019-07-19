1447eef4e8515f8746540a36688d107be0cf18e6
Toke Reimer Skovgaard <toke@reimerskovgaard.dk>
2019-07-19 14:45:38 +0200

    Added logging

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import morgan from 'morgan';
 
 // Create a new express application instance
 const app: express.Application = express();
@@ -7,6 +8,7 @@ const app: express.Application = express();
 //     res.send('Hello World!');
 // });
 app.use(express.static('public'));
+app.use(morgan('tiny'));
 
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!');
